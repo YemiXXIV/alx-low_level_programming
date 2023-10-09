5fdda1f9f857e9b9dbc574b4186f42b2f563fe2c
Mustapha <mustapha.yinusa@yahoo.com>
2023-10-09 11:30:46 +0100

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ ** main - Entry point of the program
+ **
+ ** Return: Always 0 (success)
+ **/
+int main(void)
+{char lowercase = 'z';
+while (lowercase >= 'a')
+{putchar(lowercase);
+lowercase--; }
+putchar ('\n');
+return (0); }
