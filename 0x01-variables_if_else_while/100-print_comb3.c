b6fb55d2000bf680775fb4101f71bca0dc0685b8
Mustapha <mustapha.yinusa@yahoo.com>
2023-10-09 14:37:02 +0100

    comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,7 @@ for (firstnumber = 0; firstnumber <= 8; firstnumber++)
 {for
 (secondnumber = firstnumber + 1;
 secondnumber <= 9;
-secondnumber++);
+secondnumber++)
 {putchar(firstnumber + '0');
 putchar(secondnumber + '0');
 if (firstnumber != 8 || secondnumber != 9)
