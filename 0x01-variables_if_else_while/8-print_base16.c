8b44f7b24b72f182cd1d04da824d98aa2ee539a4
Mustapha <mustapha.yinusa@yahoo.com>
2023-10-09 12:10:35 +0100

    Base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
+
 /**
- ** main - Entry point of the program
+ * main - Entry point of the program
  **
- ** Return; Always 0 (success)
+ ** Return: Always 0 (success)
  **/
 int main(void)
-{ int number = '0';
-char letter = 'a';
-while (number < 10, letter <= 'f')
-{putchar (number + '0');
-number++; }
-{putchar (letter); }
-putchar ('\n');
+{char item;
+for (item = '0'; item <= '9'; item++)
+{putchar (item); }
+for (item = 'a'; item <= 'f'; item++)
+{putchar(item); }
+putchar('\n');
 return (0);
 }
