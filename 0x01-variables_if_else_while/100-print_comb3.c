ad2c6fad304366c2ffde28a69ab0e945e1f61eb2
Mustapha <mustapha.yinusa@yahoo.com>
2023-10-09 14:25:16 +0100

    Comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ **
+ ** Return: Always 0 (success)
+ **/
+int main(void)
+{int firstnumber, secondnumber;
+for (firstnumber = 0; firstnumber <= 8; firstnumber++)
+{for
+(secondnumber = firstnumber + 1 && secondnumber <= 9;
+secondnumber++;)
+{putchar(firstnumber + '0');
+putchar(secondnumber + '0');
+if (firstnumber != 8 || secondnumber != 9)
+{putchar(',');
+putchar(' '); } } }
+putchar('\n');
+return (0); }
