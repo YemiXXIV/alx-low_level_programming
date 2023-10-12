56cf31a4720b89d863ab777f60bdaef9727e55b8
Mustapha <mustapha.yinusa@yahoo.com>
2023-10-12 16:28:09 +0100

    New changes

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Addition of two integers.
+ * @a: first value.
+ * @b: second value.
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
