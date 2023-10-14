5930b78a8d222fa00c94445bbbf4b4bad40f2bc6
Mustapha <mustapha.yinusa@yahoo.com>
2023-10-14 15:19:22 +0100

    0-mainn

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positve or negative
+ * main - test function that prints if integer is positve or negative
  * Return: 0
  */
-
 int main(void)
 {
 int i;
