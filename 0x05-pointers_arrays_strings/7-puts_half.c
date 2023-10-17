2b2d06d16dd81cdb47f9d5f9e542c1c938179711
Mustapha <mustapha.yinusa@yahoo.com>
2023-10-17 19:17:44 +0100

    7-puts

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,20 +10,28 @@ void puts_half(char *str)
 {
 	int length = 0;
 	int i = 0;
-	int start = (length - 1) / 2;
 
 	while (str[length] != '\0')
 	{
 		length++;
 	}
 
-	while (str[i] != '\0')
+	int start;
+
+	if (length % 2 == 0)
 	{
-		if (i >= start)
-		{_putchar(str[i]);
-		}
-		i++;
+		start = length / 2;
 	}
 
-	_putchar('\n');
+	else
+	{
+		start = (length - 1) / 2;
+	}
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+	}
+	i++;
+}
+_putchar('\n');
 }
