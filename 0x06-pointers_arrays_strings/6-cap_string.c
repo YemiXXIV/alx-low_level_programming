17a42a967e03af8463f30cdda4567e08f3fa494e
Mustapha <mustapha.yinusa@yahoo.com>
2023-10-21 00:41:42 +0100

    7-leet

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * cap_string - A function that capitalizes all words of a string
+ *@str: A pointer to the input string
+ *
+ * Return: A pointer to the modified string.
+ */
+
+char *cap_string(char *str)
+{
+	int c = 1;
