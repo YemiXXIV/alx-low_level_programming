2b2ee9af1e3931ce39ba4607e9296760792ffd53
Mustapha <mustapha.yinusa@yahoo.com>
2023-10-26 14:38:29 +0100

    5-sqrt_recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -17,18 +17,18 @@ int _sqrt_recursion(int n)
 	}
 
 	{
-		return (sqrt_recursive(n, 0));
+		return (_sqrt_recursive(n, 0));
 	}
 
 }
 /**
- * sqrt_recursive - Recursive function to find the square root.
+ * _sqrt_recursive - Recursive function to find the square root.
  * @n: Number to find the square root
  * @g: Square root guesses
  *
  * Return: g if the square root if found, -1 if not
  */
-	int sqrt_recursive(int n, int g)
+	int _sqrt_recursive(int n, int g)
 	{
 		if (g * g > n)
 		{
@@ -41,6 +41,6 @@ int _sqrt_recursion(int n)
 		}
 
 		{
-			return (sqrt_recursive(n, g + 1));
+			return (_sqrt_recursive(n, g + 1));
 		}
 	}
