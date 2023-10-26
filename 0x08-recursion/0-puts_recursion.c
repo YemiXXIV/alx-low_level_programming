7deb107b3901820e40591ccf586f54f00991398c
Mustapha <mustapha.yinusa@yahoo.com>
2023-10-26 11:59:36 +0100

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - A function that prints a string
+ * @s: String to be printed
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+
+	putchar(*s);
+	_puts_recursion(s + 1);
+}
+	
