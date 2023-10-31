73d3967d499c8ce4400a2820891887c37587f9f3
Mustapha <mustapha.yinusa@yahoo.com>
2023-10-31 01:02:57 +0100

    2-args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints all arguments it receives
+ * @argc: number of arguments passed
+ * @argv: Array of argument passed
+ *
+ * Return: 0 (Always success)
+ */
+
+int main(int argc, char *argv[])
+{
+	for (int i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
