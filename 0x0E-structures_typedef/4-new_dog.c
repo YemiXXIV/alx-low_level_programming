3247729094d5989cc0a72216c47d71267d296a98
Mustapha <mustapha.yinusa@yahoo.com>
2023-11-08 00:27:33 +0100

    4-new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,31 +15,43 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	char *n, *o;
+	int i = 0, j = 0, k, l;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
+	while (name[i] != '\0')
+	{
+		i++;
+	}
+	while (owner[j] != '\0')
+	{
+		j++;
+	}
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-
-	new_dog->name = strdup(name);
-	if (new_dog->name == NULL)
+	n = malloc(sizeof(char) * i + 1);
+	if (n == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-
+	o = malloc(sizeof(char) * j + 1);
+	if (o == NULL)
+	{
+		free(new_dog);
+		free(a);
+		return (NULL);
+	}
+	for (k = 0; k <= i; k++)
+		n[k] = name[k];
+	for (l = 0; l <= j; l++)
+		o[l] = owner[l];
+	new_dog->name = n;
 	new_dog->age = age;
-
-	new_dog->owner = strdup(owner);
-
-	if (new_dog->owner == NULL)
-	{
-		free(new_dog->name);
-		free(new_dog);
-		return (NULL);
-	}
+	new_dog->owner = 0;
 
 	return (new_dog);
 }
