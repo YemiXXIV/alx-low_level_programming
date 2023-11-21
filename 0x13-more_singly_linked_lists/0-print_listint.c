5cb30eee21ae4e966a9958e0ea565625669252d9
Mustapha <mustapha.yinusa@yahoo.com>
2023-11-21 18:08:45 +0100

    0-print_listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,13 +10,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t node_count = 0;
+	int node_count = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		h = h->next;
 		node_count++;
+		h = h->node;
 	}
 
 	return (node_count);
