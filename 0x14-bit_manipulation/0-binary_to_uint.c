74c517ed07477bf9d302abe5ca2f9cbc12476d82
Mustapha <mustapha.yinusa@yahoo.com>
2023-12-01 13:38:11 +0100

    0-binary_to_unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,18 +11,18 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
+	int result = 0, i = 0;
 
 	if (b == NULL)
 		return (0);
 
-	while (*b != '\0')
+	while (b[i] != '\0')
 	{
-		if (*b != '0' && *b != '1')
+		if (b[i] != '0' && b[1] != '1')
 			return (0);
 
-		result = (result << 1) + (*b - '0');
-		b++;
+		result = result * 2 + (b[i] - '0');
+		i++;
 	}
 
 	return (result);
